5ebb1e4bded0a2e7b4857432611b779f76c8a31e
Big D <shawnshaq@gmail.com>
2025-10-08 23:00:50 -0400

    chore: source update

--- deploy.ps1
+++ deploy.ps1
@@ -1,26 +1,24 @@
-# from your project root
+# 0) Make sure main is up to date
+git add -A
+git commit -m "chore: source update"
+git push -u origin main
+
+# 1) Build production
 ng build --configuration production
 
-git fetch origin
+# 2) Create a local 'deploy' worktree (do NOT reference origin/deploy yet)
+git worktree add -B deploy .deploy_publish
 
-# create the worktree (use remote deploy if it exists)
-$hasRemoteDeploy = (git ls-remote --heads origin deploy) -ne ""
-if ($hasRemoteDeploy) {
-  git worktree add -B deploy .deploy_publish origin/deploy
-} else {
-  git worktree add -B deploy .deploy_publish
-}
-
-# wipe worktree (except .git) and copy build output
+# 3) Populate worktree with the build
 Get-ChildItem -Force .deploy_publish | Where-Object { $_.Name -ne ".git" } | Remove-Item -Recurse -Force
 Copy-Item -Path dist\*\browser\* -Destination .deploy_publish -Recurse -Force
 
-# commit + push deploy branch
+# 4) Commit + push the deploy branch
 cd .deploy_publish
 git add -A
-git commit -m "deploy: production build"
+git commit -m "deploy: first build"
 git push -u origin deploy
 cd ..
 
-# optional: remove local worktree folder
+# 5) (Optional) remove the worktree folder
 git worktree remove .deploy_publish -f
