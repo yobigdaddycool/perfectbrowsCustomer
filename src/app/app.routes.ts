9f17a2586ba91c37723c5f3d34faefbd24b4b099
Big D <shawnshaq@gmail.com>
2025-10-15 08:35:46 -0400

    update db button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 ﻿import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent), title: 'Register' },
+  { path: '', loadComponent: () => import('./pages/testing-db/testing-db.component').then(m => m.TestingDbComponent), title: 'Testing DB' },
   { path: 'search', loadComponent: () => import('./pages/search/search.component').then(m => m.SearchComponent), title: 'Search' },
   { path: 'scan', loadComponent: () => import('./pages/scan/scan.component').then(m => m.ScanComponent), title: 'QR Scanner' },
   { path: 'register', loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent), title: 'Register' },
