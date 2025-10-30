cabc05a3c150c0f4534e5ead013382977226ad5d
Big D <shawnshaq@gmail.com>
2025-10-30 10:21:36 -0400

    update db button

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'register/:id',
+    renderMode: RenderMode.Server // Use SSR for dynamic routes, not prerendering
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
