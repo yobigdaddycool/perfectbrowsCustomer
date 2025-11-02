f93fd84fa85ab86a5602c19eea3e504caf24f1d0
Big D <shawnshaq@gmail.com>
2025-11-02 17:31:12 -0500

    deploy: camera button update

--- db-config.php
+++ db-config.php
@@ -2,8 +2,7 @@
 // db-config.php - Database configuration
 $host = '50.6.108.147';
 $port = '3306';
-$dbname = '
-';
+$dbname = 'ichrqhmy_PerfectCustomer';
 $username = 'ichrqhmy_testuser';
 $password = 'Destruction123!';
 
